b1cd145cc6c9f60f41ad6583606ebdd3615b9cde
coolsuraj1234 <64766133+coolsuraj1234@users.noreply.github.com>
2024-06-09 01:30:58 +0530

    Update Palindrome_number.py

--- Palindrome_number.py
+++ Palindrome_number.py
@@ -3,11 +3,8 @@ temp = number
 x = 0
 while number > 0 :
     y = number%10
-    print(y)
     x = (x*10)+y
-    print(x)
     number = number//10
-    print(number)
 if temp == x:
     print('Palindrome')
 else:
